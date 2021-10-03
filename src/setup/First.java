fc8d6a35c17844cee4cf6b4609f1b57252b49c67
mafe16 <mariefer16@hotmail.com>
2021-10-03 11:31:51 -0400

    first change

--- src/setup/First.java
+++ src/setup/First.java
@@ -3,6 +3,17 @@ package setup;
 public class First {
     public static void main(String[] args) {
         System.out.println("hello word");
+
+
+
+
+
+        //i want to create a new line
+
+        //  i want to create a project
+
+        //i want to create history
+
     }
 
 
